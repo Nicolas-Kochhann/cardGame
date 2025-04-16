64f8e694a98449205e4e5b7e2c11c69c88f83e8b
Nicolas-Kochhann <nicolas.kochhann@aluno.feliz.ifrs.edu.br>
2025-04-15 21:43:35 -0300

    Update card.js

--- card.js
+++ card.js
@@ -6,4 +6,13 @@ class Card{
     attack;
     defense;
 
+    constructor(id, name, image, attack, defense){
+        this.id = id;
+        this.name = name;
+        this.image = image;
+        this.attack = attack;
+        this.defense = defense;
+    }
 }
+
+let card1 = new Card(1, "card1", "/resources/card.png", 5, 5)
